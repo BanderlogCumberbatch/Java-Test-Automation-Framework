c743b3f8d5235bafae40b663d3578650572bd7cb
BanderlogCumberbatch <kutlub.artur@gmail.com>
2025-05-27 19:41:52 +0300

    undo

--- src/main/java/org/pages/bank/OpenAccountPage.java
+++
@@ -1,9 +0,0 @@
-package org.pages.bank;
-
-import org.openqa.selenium.WebDriver;
-
-public class OpenAccountPage extends BankManagerPage {
-
-    public OpenAccountPage(final WebDriver webDriver) { super(webDriver); }
-
-}
